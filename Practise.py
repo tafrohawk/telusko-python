b4e7ae7b85c807ca8d6486c6bf9e1a937cb7ebad
Tomas Frohawk <tom@email.com>
2020-05-01 00:21:18 +0100

    minor changes

--- Practise.py
+++ Practise.py
@@ -17,7 +17,9 @@
 #data = {1:'Navin', 2:'Kiran', 4:'Harsh'}
 
 # merge keys and values into dictionary
-keys = ['Navin', 'Kiran', 'Harsh']
-values = ['Python', 'Java', 'JS']
-data = dict(zip(keys, values))
-print(data)
+# keys = ['Navin', 'Kiran', 'Harsh']
+# values = ['Python', 'Java', 'JS']
+# data = dict(zip(keys, values))
+# print(data)
+
+
